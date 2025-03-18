41f2eaa483d5cb9f8f05c53bb3cae1675e34a8d5
Alexander Koshkej <61204471+Koshkej@users.noreply.github.com>
2025-03-18 03:11:27 +0300

    Initial

--- main.cpp
+++ main.cpp
@@ -2,38 +2,30 @@
 #include <cmath>
 #include <numbers>
 #include <vector>
-
 #include "SFML/Graphics.hpp"
-
 #include "Sphere.h"
 
 int main() {
+    sf::RenderWindow window(sf::VideoMode(900, 900), "Sphere", sf::Style::Close);
+    sf::Event event;
+    Sphere sph1(sf::Vector2f(450, 450), 200, 32);
 
-	sf::RenderWindow window(sf::VideoMode(900, 900), "Sphere", sf::Style::Close);
-	sf::Event defaultEvent;
+    while (window.isOpen()) {
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed ||
+                (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape)) {
+                window.close();
+            }
+            if (event.type == sf::Event::KeyPressed) {
+                if (event.key.code == sf::Keyboard::Right) sph1.HorizontalMove(-2);
+                if (event.key.code == sf::Keyboard::Left) sph1.HorizontalMove(+2);
+            }
+        }
 
-	Sphere sph1(sf::Vector2f(450, 450), 200, 32);
+        window.clear();
+        sph1.renderPolygons(&window);
+        window.display();
+    }
 
-	while (window.isOpen()) {
-
-		window.clear();
-
-		if (window.pollEvent(defaultEvent)) {
-			if (defaultEvent.type == sf::Event::Closed
-				|| (defaultEvent.type == sf::Event::KeyPressed && sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)))
-					window.close();
-			if (defaultEvent.type == sf::Event::KeyPressed && sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
-				sph1.HorizontalMove(-2);
-			if (defaultEvent.type == sf::Event::KeyPressed && sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
-				sph1.HorizontalMove(+2);
-		}
-
-		sph1.renderPolygons(&window);
-		
-		window.display();
-		
-	}
-
-	return 0;
-
+    return 0;
 }
